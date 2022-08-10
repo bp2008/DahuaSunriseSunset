1c285e37b6fac08211808269982970b14c52cbd5
bp2008 <bp2008@users.noreply.github.com>
2022-08-10 12:58:12 -0600

    Added a graph of all sunrise and sunset times for the next year, to the "View Next" panel, to aid in troubleshooting.

--- DahuaSunriseSunset/ViewNextSunriseSunset.Designer.cs
+++ DahuaSunriseSunset/ViewNextSunriseSunset.Designer.cs
@@ -29,26 +29,108 @@
 		private void InitializeComponent()
 		{
 			this.label1 = new System.Windows.Forms.Label();
+			this.pbSunriseSunsetGraph = new System.Windows.Forms.PictureBox();
+			this.pbDay = new System.Windows.Forms.PictureBox();
+			this.pbNight = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.lblComputingYear = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pbSunriseSunsetGraph)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbDay)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbNight)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
-			this.label1.AutoSize = true;
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label1.Location = new System.Drawing.Point(12, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.Size = new System.Drawing.Size(369, 173);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "label1";
+			// 
+			// pbSunriseSunsetGraph
+			// 
+			this.pbSunriseSunsetGraph.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.pbSunriseSunsetGraph.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbSunriseSunsetGraph.Location = new System.Drawing.Point(15, 185);
+			this.pbSunriseSunsetGraph.Name = "pbSunriseSunsetGraph";
+			this.pbSunriseSunsetGraph.Size = new System.Drawing.Size(366, 240);
+			this.pbSunriseSunsetGraph.TabIndex = 1;
+			this.pbSunriseSunsetGraph.TabStop = false;
+			// 
+			// pbDay
+			// 
+			this.pbDay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.pbDay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.pbDay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbDay.Location = new System.Drawing.Point(87, 431);
+			this.pbDay.Name = "pbDay";
+			this.pbDay.Size = new System.Drawing.Size(20, 20);
+			this.pbDay.TabIndex = 2;
+			this.pbDay.TabStop = false;
+			// 
+			// pbNight
+			// 
+			this.pbNight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.pbNight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			this.pbNight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbNight.Location = new System.Drawing.Point(244, 431);
+			this.pbNight.Name = "pbNight";
+			this.pbNight.Size = new System.Drawing.Size(20, 20);
+			this.pbNight.TabIndex = 3;
+			this.pbNight.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(113, 435);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(26, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Day";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(270, 435);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(32, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Night";
+			// 
+			// lblComputingYear
+			// 
+			this.lblComputingYear.AutoSize = true;
+			this.lblComputingYear.Location = new System.Drawing.Point(82, 299);
+			this.lblComputingYear.Name = "lblComputingYear";
+			this.lblComputingYear.Size = new System.Drawing.Size(242, 13);
+			this.lblComputingYear.TabIndex = 6;
+			this.lblComputingYear.Text = "Computing Sunrise/Sunset graph for next 1 year...";
 			// 
 			// ViewNextSunriseSunset
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(405, 147);
+			this.ClientSize = new System.Drawing.Size(399, 462);
+			this.Controls.Add(this.lblComputingYear);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pbNight);
+			this.Controls.Add(this.pbDay);
+			this.Controls.Add(this.pbSunriseSunsetGraph);
 			this.Controls.Add(this.label1);
 			this.Name = "ViewNextSunriseSunset";
 			this.Text = "Next Sunrise and Sunset";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ViewNextSunriseSunset_FormClosing);
 			this.Load += new System.EventHandler(this.ViewNextSunriseSunset_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pbSunriseSunsetGraph)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbDay)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbNight)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -57,5 +139,11 @@
 		#endregion
 
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pbSunriseSunsetGraph;
+		private System.Windows.Forms.PictureBox pbDay;
+		private System.Windows.Forms.PictureBox pbNight;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label lblComputingYear;
 	}
 }
--- DahuaSunriseSunset/ViewNextSunriseSunset.cs
+++ DahuaSunriseSunset/ViewNextSunriseSunset.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using BPUtil;
+using CoordinateSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +20,7 @@ namespace DahuaSunriseSunset
 		{
 			InitializeComponent();
 		}
-
+		BackgroundWorker bw;
 		private void ViewNextSunriseSunset_Load(object sender, EventArgs e)
 		{
 			DahuaSunriseSunsetConfig cfg = new DahuaSunriseSunsetConfig();
@@ -38,6 +42,163 @@ namespace DahuaSunriseSunset
 				:
 				("Sunrise at " + rise + OffsetStr(cfg.sunriseOffsetHours) + Environment.NewLine
 				+ "Sunset at " + set + OffsetStr(cfg.sunsetOffsetHours)));
+
+			bw = new BackgroundWorker();
+			bw.DoWork += Bw_DoWork;
+			bw.RunWorkerCompleted += Bw_RunWorkerCompleted;
+			bw.WorkerSupportsCancellation = true;
+			bw.RunWorkerAsync(cfg);
+		}
+
+		private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			bw = null;
+			if (e.Result == null)
+			{
+				lblComputingYear.Text = "Cancelled...";
+				return;
+			}
+			lblComputingYear.Visible = false;
+			pbSunriseSunsetGraph.BorderStyle = BorderStyle.None;
+			pbSunriseSunsetGraph.Image = ((dynamic)e.Result).bmp as Bitmap;
+			BasicEventTimer bet = ((dynamic)e.Result).timing as BasicEventTimer;
+			label1.Text += Environment.NewLine + Environment.NewLine + "It took " + bet.GetEventTime(0).TotalMilliseconds.ToString("0") + "ms to compute Sunrise/Sunset times for 1 year, and " + bet.GetEventTime(1).TotalMilliseconds.ToString("0") + "ms to render the graph.";
+		}
+
+		private void Bw_DoWork(object sender, DoWorkEventArgs e)
+		{
+			DahuaSunriseSunsetConfig cfg = (DahuaSunriseSunsetConfig)e.Argument;
+			try
+			{
+				BasicEventTimer bet = new BasicEventTimer();
+				bet.Start("Sunrise/Sunset Computations");
+				DateTime startDate = DateTime.Today;
+				DateTime start = startDate;
+				DateTime end = startDate.AddYears(1);
+				List<SunriseSunsetEvent> events = new List<SunriseSunsetEvent>();
+				while (start < end)
+				{
+					if (e.Cancel)
+						return;
+					Coordinate c = new Coordinate(cfg.latitude, cfg.longitude, start);
+					c.Offset = TimeZone.CurrentTimeZone.GetUtcOffset(start).TotalHours;
+					DateTime nextRise = Celestial.Get_Next_SunRise(c);
+					DateTime nextSet = Celestial.Get_Next_SunSet(c);
+					if (nextRise > nextSet)
+					{
+						events.Add(new SunriseSunsetEvent(nextSet, false));
+						events.Add(new SunriseSunsetEvent(nextRise, true));
+						start = nextRise.AddSeconds(1);
+					}
+					else
+					{
+						events.Add(new SunriseSunsetEvent(nextRise, true));
+						events.Add(new SunriseSunsetEvent(nextSet, false));
+						start = nextSet.AddSeconds(1);
+					}
+				}
+				// Add fake event much later so that the graph rendering will complete correctly.
+				events.Add(new SunriseSunsetEvent(events[events.Count - 1].time.AddYears(1), !events[events.Count - 1].isRise));
+				bet.Start("Graph Rendering");
+				Bitmap bmp = new Bitmap(366, 240, PixelFormat.Format24bppRgb);
+				List<AxisLabel> xAxisLabels = new List<AxisLabel>();
+				List<AxisLabel> yAxisLabels = new List<AxisLabel>();
+				if (events.Count > 0)
+				{
+					int i = 0;
+					for (int x = 0; x < bmp.Width; x++)
+					{
+						for (int y = 0; y < bmp.Height; y++)
+						{
+							DateTime pixelTime = startDate.AddDays(x).AddHours(y / 10.0);
+							if (events[i].time < pixelTime)
+								i++;
+							if (y > bmp.Height - 6 && pixelTime.Day == 1)
+							{
+								// This pixel is a tickmark on the y-axis.
+								if (y == bmp.Height - 5)
+									xAxisLabels.Add(new AxisLabel(x, y, pixelTime.Month.ToString()));
+								bmp.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+							}
+							else if (x < 6 && y > 0 && y % 10 == 0)
+							{
+								// This pixel is a tickmark on the x-axis.
+								if (x == 5)
+									yAxisLabels.Add(new AxisLabel(x, y, pixelTime.Hour.ToString()));
+								bmp.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+							}
+							else
+							{
+								bool isDay = !events[i].isRise;
+								if (isDay)
+									bmp.SetPixel(x, y, Color.FromArgb(255, 255, 128));
+								else
+									bmp.SetPixel(x, y, Color.FromArgb(128, 128, 255));
+							}
+						}
+					}
+				}
+
+				Graphics g = Graphics.FromImage(bmp);
+
+				Font yAxisFont = new Font("Tahoma", 6);
+				Font xAxisFont = new Font("Tahoma", 8);
+				g.SmoothingMode = SmoothingMode.AntiAlias;
+				g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+				g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+				PointF upperLeftDrawnPoint = PointF.Empty;
+				foreach (AxisLabel lbl in xAxisLabels)
+				{
+					SizeF size = g.MeasureString(lbl.label, xAxisFont);
+					float x = lbl.x - (size.Width / 2) + 1;
+					float y = lbl.y - size.Height;
+					g.DrawString(lbl.label, xAxisFont, Brushes.Black, x, y);
+					if (upperLeftDrawnPoint.IsEmpty)
+						upperLeftDrawnPoint = new PointF(x, y);
+				}
+				foreach (AxisLabel lbl in yAxisLabels)
+				{
+					SizeF size = g.MeasureString(lbl.label, yAxisFont);
+					float x = lbl.x + 1;
+					float y = lbl.y - (size.Height / 2) + 1;
+					if (x + size.Width <= upperLeftDrawnPoint.X || y + size.Height <= upperLeftDrawnPoint.Y)
+						g.DrawString(lbl.label, yAxisFont, Brushes.Black, x, y);
+				}
+				g.Flush();
+				bet.Stop();
+				pbSunriseSunsetGraph.Image = bmp;
+				e.Result = new { bmp, timing = bet };
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+
+		class SunriseSunsetEvent
+		{
+			public DateTime time;
+			public bool isRise;
+
+			public SunriseSunsetEvent(DateTime time, bool isRise)
+			{
+				this.time = time;
+				this.isRise = isRise;
+			}
+		}
+		class AxisLabel
+		{
+			public int x;
+			public int y;
+			public string label;
+
+			public AxisLabel(int x, int y, string label)
+			{
+				this.x = x;
+				this.y = y;
+				this.label = label;
+			}
 		}
 		private static string OffsetStr(double hours)
 		{
@@ -45,6 +206,11 @@ namespace DahuaSunriseSunset
 				return "";
 			else
 				return " (time was offset " + hours + " hr)";
+		}
+
+		private void ViewNextSunriseSunset_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			bw?.CancelAsync();
 		}
 	}
 }
